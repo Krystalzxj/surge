8efd82c51a2487aaa03597cdbb7509ad45edf4cb
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-05 08:49:41 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -38,7 +38,8 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`使用：${bytesToSize(used)} | 总量：${bytesToSize(total)}/n` + `剩余：${bytesToSize(total-used)}`];
+  let content = [`使用：${bytesToSize(used)} | 总量：${bytesToSize(total)}/n` + 
+`剩余：${bytesToSize(total-used)}`];
   if (resetDayLeft) {
     content.push(`重置：剩余${resetDayLeft}天`);
   }
