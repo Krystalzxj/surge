77116b8e85a21166d69351916736c1b604db6d5e
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-10 23:04:39 +0800

    Update function_timeTransform.js

--- function_timeTransform.js
+++ function_timeTransform.js
@@ -21,7 +21,7 @@ if ($trigger == "button") {
 };
 $done({
     title:"Surge Pro® ✌(՞ټ՞ )✌",
-    content: "北京时间:"+(myDate.toLocaleString()).Format("yyyy-MM-dd hh:mm:ss")+"\n❦启动☞时长❥："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
+    content: "北京时间:"+myDate.toLocaleString()+"\n❦启动☞时长❥："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"exclamationmark.triangle",
    "icon-color":icon_s?"#f6c970":"#FF7500"
 });
