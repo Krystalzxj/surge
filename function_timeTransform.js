1629ea4b2c131730e7c20437e73efa4edbcb16b2
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-10 20:43:22 +0800

    Update function_timeTransform.js

--- function_timeTransform.js
+++ function_timeTransform.js
@@ -19,7 +19,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"Surge Pro® ✌(՞ټ՞ )✌",
+    title:"Surge Pro®"+ (new Date()).Format("yyyy-MM-dd hh:mm:ss"),
     content:"启动时长："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"exclamationmark.triangle",
    "icon-color":icon_s?"#f6c970":"#FF7500"
