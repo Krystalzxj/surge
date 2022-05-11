e4091e61f9403477b214a10e0b557276086b1a1c
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-11 22:32:51 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -30,22 +30,6 @@ Sub_info = script-name=Sub_info,update-interval=600
 
 let args = getArgs();
 
-Date.prototype.Format = function (fmt) {
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "H+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-}
-
 (async () => {
   let info = await getDataInfo(args.url);
   if (!info) $done();
@@ -71,7 +55,7 @@ Date.prototype.Format = function (fmt) {
   minutes = minutes > 9 ? minutes : "0" + minutes;
   seconds = seconds > 9 ? seconds : "0" + seconds;
   $done({
-    title: `${args.title} |`+ new Date().Format("yyyy-MM-dd")+` ${hour}:${minutes}:${seconds}`,
+    title: `${args.title} |⏰${hour}:${minutes}:${seconds}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
