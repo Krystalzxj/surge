94a69e8262ba6a8769bfc8a4651c30685d443de0
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-11 22:59:04 +0800

    Update functionstatus.js

--- functionstatus.js
+++ functionstatus.js
@@ -34,7 +34,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"Surge👑Pro | ⏰："+ new Date().Format("HH:mm:ss"),
+    title:"Surge♛Pro|new Date().Format("yyyy-MM-dd HH:mm:ss"),
     content: "启动时长："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"exclamationmark.triangle",
    "icon-color":icon_s?"#EACD76":"#F20C00"
