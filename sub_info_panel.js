fed75c7c84496251f5b48ed73b943568d54aca27
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-11 21:40:48 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -55,7 +55,7 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
   seconds = seconds > 9 ? seconds : "0" + seconds;
   $done({
-    title: `${args.title} | `now.toLocaleDateString() `${hour}:${minutes}:${seconds}`,
+    title: `${args.title} | `+now.toLocaleDateString()+ `${hour}:${minutes}:${seconds}`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
