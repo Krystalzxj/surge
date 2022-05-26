0a42562cb855e25d338fd7c858790e94f0e2241c
Krystalzxj <103553035+Krystalzxj@users.noreply.github.com>
2022-05-26 22:12:38 +0800

    Update function_timeTransform.js

--- function_timeTransform.js
+++ function_timeTransform.js
@@ -34,10 +34,10 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"Surge👑Pro™ ",
+    title:"«𝑺𝒖𝒓𝒈𝒆👑𝑷𝒓𝒐™» 🧚🏻‍♀️ ➺ ✌(՞ټ՞ )✌",
     content: "北京时间："+ (new Date()).Format("yyyy-MM-dd HH:mm:ss")+"\n启动时长："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"exclamationmark.triangle",
-   "icon-color":icon_s?"#EACD76":"#F20C00"
+   "icon-color":icon_s?"#EACD76":"#f6c970"
 });
 })();
 function icon_status(status){
