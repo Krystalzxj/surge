280850c15c156b8b90c6bea33a0d6777e3a4fcf2
Krystalzxj <103553035+Krystalzxj@users.noreply.github.com>
2022-05-26 17:22:58 +0800

    Update stream.js

--- stream.js
+++ stream.js
@@ -51,7 +51,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       } else if (status==STATUS_AVAILABLE){
         //console.log(2)
         console.log(region)
-        disney_result="Disney+: 已解锁 ➺ " +`${getFlagEmoji(region)} | ` + region.toUpperCase()
+        disney_result="𝑫𝒊𝒔𝒏𝒆𝒚+: 已解锁 ➺ " +`${getFlagEmoji(region)} | ` + region.toUpperCase()
         // console.log(result["Disney"])
       } else if (status==STATUS_NOT_AVAILABLE) {
         //console.log(3)
@@ -103,7 +103,7 @@ panel_result['content'] = content
       })
     }
   
-    let youtube_check_result = 'YouTube: '
+    let youtube_check_result = '𝒀𝒐𝒖𝑻𝒖𝒃𝒆: '
   
     await inner_check()
       .then((code) => {
@@ -159,7 +159,7 @@ panel_result['content'] = content
       })
     }
   
-    let netflix_check_result = 'Netflix: '
+    let netflix_check_result = '𝑵𝒆𝒕𝒇𝒍𝒊𝒙: '
   
     await inner_check(81215567)
       .then((code) => {
