9e2116b02272ebd547dd92cc15c1fd4609a53301
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-05-27 21:49:00 +0800

    Update profile_reload.js

--- profile_reload.js
+++ profile_reload.js
@@ -10,7 +10,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
    minutes = minutes > 9 ? minutes : "0" + minutes;
    seconds = seconds > 9 ? seconds : "0" + seconds;
     $done({
-        title:"配置重载 🍑🍗🍔🍟🍩🍯",
+        title:"配置重载 🔄",
         content:"🕰 𝑳𝒂𝒔𝒕 𝒓𝒖𝒏𝒕𝒊𝒎𝒆 :  "+hour+":"+minutes+":"+seconds,
         icon:"leaf.arrow.triangle.circlepath",
         "icon-color": "#00E500",
